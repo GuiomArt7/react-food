7a9b764c8d7882267c8156afd96948417e575487
GuiomArt7 <ada_hack243@hackademy.mx>
2024-02-29 13:09:45 -0600

    feat: añadir funcionalidad de ajustar cantidades de productos para el pedido

--- src/components/ModalProducto.jsx
+++ src/components/ModalProducto.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 import useMenu from "../hooks/useMenu"
 import {formatearDinero} from "../helpers";
 
 export default function ModalProducto() {
 
     const {producto, handleClickModal} = useMenu();
+    const [cantidad, setCantidad] = useState(1);
+
   return (
-    <div className="md:flex gap-10">
+    <div className="md:flex items-center gap-10">
         <div className="md:w-1/3">
             <img className="w-80"
             src={`/img/${producto.imagen}.jpg`}
@@ -27,6 +30,31 @@ export default function ModalProducto() {
           <p className="mt-5 font-black text-5xl text-amber-500">
             {formatearDinero(producto.precio)}
           </p>
+
+          <div className="flex gap-4 mt-5">
+            <button
+            type="button"
+            onClick={() => {
+              if(cantidad <= 1) return
+              setCantidad(cantidad - 1)
+            }}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              </svg>
+            </button>
+            <p className="text-3xl">{cantidad}</p>
+            <button
+            type="button"
+            onClick={() => {
+              if(cantidad >= 10) return
+              setCantidad(cantidad + 1)
+            }}>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              </svg>
+            </button>
+          </div>
+
           <button
           type="button"
           className="bg-cyan-800 hover:bg-cyan-950 px-5 py-2 mt-5 text-white font-bold uppercase rounded">
