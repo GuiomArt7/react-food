7c4b6261f837c3e1d10b834a6fca5b1e63be3254
GuiomArt7 <ada_hack243@hackademy.mx>
2024-03-04 23:14:37 -0600

    feat: enviar la petición de registro hacia el controlador

--- src/views/Registro.jsx
+++ src/views/Registro.jsx
@@ -1,90 +1,114 @@
-import {Link} from 'react-router-dom'
-import { Outlet } from 'react-router-dom'
+import { createRef, useState } from "react";
+import { Link } from "react-router-dom";
+import clienteAxios from "../config/axios";
+//import { Outlet } from 'react-router-dom'
 
 export default function Registro() {
+  const nameRef = createRef();
+  const emailRef = createRef();
+  const passwordRef = createRef();
+  const passwordConfirmationRef = createRef();
+
+  const handleSubmit = async e => {
+    e.preventDeafault();
+
+    const datos = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      password: passwordRef.current.value,
+      password_confirmation: passwordConfirmationRef.current.value,
+    }
+    try {
+      const respuesta = await clienteAxios.post('/api/registro', datos)
+      console.log(respuesta)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
-      <h1 className='text-4xl font-black'>Crea tu cuenta</h1>
+      <h1 className="text-4xl font-black">Crea tu cuenta</h1>
       <p>Llena este formulario</p>
 
       <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
-        <form action="">
+        <form 
+        onSubmit={handleSubmit}
+        action="">
+
+
           {/* Nombre */}
           <div className="mb-4">
-            <label 
-            className='text-slate-800'
-            htmlFor="name"
-            >Nombre:</label>
-            <input 
-              type="text" 
-              id='name'
-              className='mt-2 w-full p-3 bg-gray-50'
-              name='name'
-              placeholder='Tu nombre'
+            <label className="text-slate-800" htmlFor="name">
+              Nombre:
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="mt-2 w-full p-3 bg-gray-50"
+              name="name"
+              placeholder="Tu nombre"
+              ref={nameRef}
             />
           </div>
 
           {/* Email */}
           <div className="mb-4">
-            <label 
-            className='text-slate-800'
-            htmlFor="email"
-            >E-mail:</label>
-            <input 
-              type="email" 
-              id='email'
-              className='mt-2 w-full p-3 bg-gray-50'
-              name='email'
-              placeholder='Tu e-mail'
+            <label className="text-slate-800" htmlFor="email">
+              E-mail:
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="mt-2 w-full p-3 bg-gray-50"
+              name="email"
+              placeholder="Tu e-mail"
+              ref={emailRef}
             />
           </div>
 
           {/* Password */}
           <div className="mb-4">
-            <label 
-            className='text-slate-800'
-            htmlFor="password"
-            >Contraseña:</label>
-            <input 
-              type="password" 
-              id='password'
-              className='mt-2 w-full p-3 bg-gray-50'
-              name='password'
-              placeholder='Tu contraseña'
+            <label className="text-slate-800" htmlFor="password">
+              Contraseña:
+            </label>
+            <input
+              type="password"
+              id="password"
+              className="mt-2 w-full p-3 bg-gray-50"
+              name="password"
+              placeholder="Tu contraseña"
+              ref={passwordRef}
             />
           </div>
 
           {/* Repetir Password */}
           <div className="mb-4">
-            <label 
-            className='text-slate-800'
-            htmlFor="password_confirmation"
-            >Repetir Contraseña:</label>
-            <input 
-              type="password" 
-              id='password_confirmation'
-              className='mt-2 w-full p-3 bg-gray-50'
-              name='password_confirmation'
-              placeholder='Repetir contraseña'
+            <label className="text-slate-800" htmlFor="password_confirmation">
+              Repetir Contraseña:
+            </label>
+            <input
+              type="password"
+              id="password_confirmation"
+              className="mt-2 w-full p-3 bg-gray-50"
+              name="password_confirmation"
+              placeholder="Repetir contraseña"
+              ref={passwordConfirmationRef}
             />
           </div>
 
-          <input 
+          <input
             type="submit"
             value="Crear cuenta"
-            className='bg-cyan-800  hover:bg-cyan-950 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer'
+            className="bg-cyan-800  hover:bg-cyan-950 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
           />
         </form>
       </div>
 
-      <nav className='mt-5'>
-      <Link to="/auth/login">
-        ¿Ya tienes una cuenta? Inicia sesión
-      </Link>
-    </nav>
-      <div>
-        <Outlet />
-      </div>
+      <nav className="mt-5">
+        <Link to="/auth/login">¿Ya tienes una cuenta? Inicia sesión</Link>
+      </nav>
+      <div>{/* <Outlet /> */}</div>
     </>
-  )
+  );
 }
