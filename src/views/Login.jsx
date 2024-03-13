ed2ee0ea042487862a7fda4ecc0850ea7638796e
GuiomArt7 <ada_hack243@hackademy.mx>
2024-03-12 18:19:10 -0600

    feat: handleSubmit de Login

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,13 +1,40 @@
-import {Link} from 'react-router-dom'
-import { Outlet } from 'react-router-dom'
+import { createRef, useState } from "react";
+import { Link } from "react-router-dom";
+import clienteAxios from "../config/axios";
+import Alerta from "../components/Alerta";
+
 export default function Login() {
+
+    const emailRef = createRef();
+    const passwordRef = createRef();
+  
+    const [errores, setErrores] = useState([])
+  
+    const handleSubmit = async e => {
+      e.preventDefault();
+  
+      const datos = {
+        email: emailRef.current.value,
+        password: passwordRef.current.value,
+      }
+      try {
+        const {data} = await clienteAxios.post('/api/login', datos)
+        console.log(data.token)
+      } catch(error) {
+        setErrores(Object.values(error.response.data.errors))
+      }
+    }
   return (
     <>
     <h1 className='text-4xl font-black'>Iniciar sesión</h1>
     <p>Para crear un pedido debes iniciar sesión</p>
 
     <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
-      <form action="">
+      <form 
+      onSubmit={handleSubmit}
+      noValidate
+      
+      >
         {/* Email */}
         <div className="mb-4">
           <label 
@@ -20,6 +47,7 @@ export default function Login() {
             className='mt-2 w-full p-3 bg-gray-50'
             name='email'
             placeholder='Tu e-mail'
+            ref={emailRef}
           />
         </div>
 
@@ -35,6 +63,7 @@ export default function Login() {
             className='mt-2 w-full p-3 bg-gray-50'
             name='password'
             placeholder='Tu contraseña'
+            ref={passwordRef}
           />
         </div>
 
