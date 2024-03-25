910723252482de99c98b108fe068458a6db0fb9e
GuiomArt7 <ada_hack243@hackademy.mx>
2024-03-25 13:43:29 -0600

    feat: Detectar cuando un Usuario es Admin

--- src/views/Ordenes.jsx
+++ src/views/Ordenes.jsx
@@ -1,6 +1,9 @@
 
 export default function Ordenes() {
   return (
-    <div>Ordenes</div>
+    <div>
+      <h1 className='text-4xl font-black'>Órdenes</h1>
+      <p className='text-2xl my-10'>Administra las órdenes desde aquí</p>
+</div>
   )
 }
