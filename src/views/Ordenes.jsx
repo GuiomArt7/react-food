3e5731e3d6560b758675ab021d4644e812defee1
GuiomArt7 <ada_hack243@hackademy.mx>
2024-03-25 14:05:21 -0600

    feat: consultar las órdenes

--- src/views/Ordenes.jsx
+++ src/views/Ordenes.jsx
@@ -1,5 +1,19 @@
+import useSWR from "swr"
+import clienteAxios from "../config/axios"
 
 export default function Ordenes() {
+
+  const token = localStorage.getItem('AUTH_TOKEN')
+  const fetcher = () => clienteAxios('/api/pedidos', {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+
+  const {data, error, isLoading} = useSWR('/api/pedidos', fetcher)
+  console.log(data)
+  console.log(error)
+  console.log(isLoading)
   return (
     <div>
       <h1 className='text-4xl font-black'>Órdenes</h1>
