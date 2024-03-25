b3933d753eb79fee6b405f984b6bc0bfc48b4291
GuiomArt7 <ada_hack243@hackademy.mx>
2024-03-25 16:42:23 -0600

    feat: Mostrar el contenido del pedido

--- src/views/Ordenes.jsx
+++ src/views/Ordenes.jsx
@@ -11,13 +11,36 @@ export default function Ordenes() {
   })
 
   const {data, error, isLoading} = useSWR('/api/pedidos', fetcher)
-  console.log(data)
-  console.log(error)
-  console.log(isLoading)
+ 
+  if(isLoading) return 'Cargando...'
   return (
     <div>
       <h1 className='text-4xl font-black'>Órdenes</h1>
       <p className='text-2xl my-10'>Administra las órdenes desde aquí</p>
-</div>
+
+      <div>
+        {data.data.data.map(pedido => (
+          <div key={pedido.id} className="p-5 bg-white shadow space-y-2 border-b">
+              <p className="text-xl font-bold text-slate-600">
+                  Contenido del pedido
+              </p>
+
+              {pedido.productos.map(producto => (
+                <div 
+                  key={producto.id}
+                  className="border-b border-b-slate-200 last-of-type:border-none py-4">
+                    <p className="text-sm">ID: {producto.id}</p>
+                    <p className="text-sm">{producto.nombre}</p>
+                    <p>
+                      Cantidad: {''}
+                      <span className="font-bold">{producto.pivot.cantidad}</span>
+                    </p>
+
+                </div>
+              ))}
+          </div>
+        ))}
+      </div>
+    </div>
   )
 }
