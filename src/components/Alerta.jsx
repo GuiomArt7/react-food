10e338515b819ff61a04e1736fd6c08ebcd6f292
GuiomArt7 <ada_hack243@hackademy.mx>
2024-05-24 14:40:14 -0600

    fix: Cambiar diseño de alerta de error

--- src/components/Alerta.jsx
+++ src/components/Alerta.jsx
@@ -1,8 +1,13 @@
 
 export default function Alerta({children}) {
   return (
-    <div className="text-center my-2 bg-red-600 text-white font-bold p-3 uppercase">
-        {children}
+    //<div className="text-center my-2 bg-red-700 text-white font-bold p-3 uppercase">
+        //{children}
+    //</div>
+    <div class="font-bold flex items-center p-2.5 mb-1 text-base text-red-900 border border-red-300 rounded-lg bg-red-200 dark:bg-red-70 dark:text-red-600 dark:border-red-800" role="alert">
+    <div>
+      {/* <span class="font-medium">Error: </span> */}{children}
     </div>
+  </div>
   )
 }
