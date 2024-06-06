2cc55fb1ad7afab598b6e82c8cb4517206299c91
GuiomArt7 <ada_hack243@hackademy.mx>
2024-06-06 11:16:37 -0600

    fix: adjust sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -9,13 +9,13 @@ export default function Sidebar() {
     const {logout, user} = useAuth({middleware: 'auth'})
   return (
     <aside className="md:w-72">
-        <div className="p-4">
-            <img  className="w-50"
+        <div className="flex justify-center p-2 items-center">
+            <img  className="w-40"
             src="img/s.svg" 
             alt="imagen logo" />
         </div>
 
-        <p className="my-10 text-xl font-bold text-center text-indigo-900">Hola {user?.name}</p>
+        <p className="my-1 text-xl font-bold text-center text-indigo-900">Hola {user?.name}</p>
 
         <div className="mt-1">
             {categorias.map( categoria => (
@@ -26,7 +26,7 @@ export default function Sidebar() {
             ))}
         </div>
 
-        <div className="my-5 py-5" >
+        <div className="py-5" >
             <button
             type="button"
             className="text-center bg-pink-900  w-full p-3 font-bold text-white truncate"
