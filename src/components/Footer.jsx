af93cf0699c4036ddd5adc544b9447eb468aa47f
GuiomArt7 <ada_hack243@hackademy.mx>
2024-07-28 00:35:54 -0600

    fix:cambiar ruta de imagen de footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <div className="w-full max-w-screen-xl mx-auto p-2  sm:flex sm:items-center sm:justify-between">
         {/* <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" /> */}
         
-        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Shalom <span><img src="../public/fav.svg" className="h-10 inline" alt="Logo" /></span></span>
+        <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Shalom <span><img src="/fav.svg" className="h-10 inline" alt="Logo" /></span></span>
         
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400"> <a href="https://github.com/GuiomArt7" className="hover:underline">GuioMart </a>© 2024 . Todos los derechos reservados.</span>
     </div>
